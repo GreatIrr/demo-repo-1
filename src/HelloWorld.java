1e15d979042b6e62c127060b35d468e9a33f1c00
iryna.bartnytska <iryna.bartnytska@ringteam.com>
2024-10-02 17:32:05 +0300

    added method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,10 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        someMethod();
+    }
+
+    private static void someMethod() {
+        System.out.println("i want to type smth here");
     }
 }
