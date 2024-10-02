98dc9f88b3bd5d9132ddd1825ae1f3e84c1b04d6
GreatIrr <32968261+GreatIrr@users.noreply.github.com>
2024-10-02 18:52:31 +0300

    nice!!!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Very nice hello main!");
+        System.out.println("Very nice hello main!!!");
     }
 }
